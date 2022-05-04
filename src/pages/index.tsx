b66d1483f6b563369ad1f54e752d466881282d46
Mateus Correia <mateus.correia@outlook.com>
2022-05-04 14:22:33 -0300

    created the document file using the Next Js and the home file style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <Head>
+     <title> Start | ig.news</title>
+    </Head>  
+    <h1 className={styles.title}>
+      Hello <span>World</span>
+    </h1>
+    </>
   )
 }
