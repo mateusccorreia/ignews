abebb5b61ffb10a0ee083a4ff7f24044c2daac12
Mateus Correia <mateus.correia@outlook.com>
2022-05-05 22:27:27 -0300

    Created the Signin button in the project!

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,15 @@
+import { FaGithub } from "react-icons/fa"
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    return (
+        <button 
+        type="button"
+        className={styles.signInButton}
+        >
+            <FaGithub color= "#eba417" />
+            Sign in with Github
+        </button>
+    );
+}
