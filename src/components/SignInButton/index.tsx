def9f9626f1d32af0b25a32d3e948756d45fd2e1
Mateus Correia <mateus.correia@outlook.com>
2022-05-06 17:30:56 -0300

    added the home page content and the styles of the page

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,29 @@
 import { FaGithub } from "react-icons/fa"
+import { FiX } from "react-icons/fi"
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    return (
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
         <button 
-        type="button"
-        className={styles.signInButton}
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color= "#04d361" />
+            Mateus Correia
+            <FiX color= "#737238" className={styles.closeIcon} />
+        </button>
+
+    ) : (
+        <button 
+            type="button"
+            className={styles.signInButton}
         >
             <FaGithub color= "#eba417" />
             Sign in with Github
         </button>
+
     );
 }
