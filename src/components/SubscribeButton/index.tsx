87919fa743eb9a8eda9cd6db4233e5b6389c29d5
Mateus Correia <mateus.correia@outlook.com>
2022-05-22 15:54:08 -0300

    creating the checkout session with stripe and fauna db

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,27 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/react';
+import { stripe } from '../../services/stripe';
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const {data: session} = useSession();
+
+    function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+    }
+    
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
