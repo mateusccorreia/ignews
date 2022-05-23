9a739882858be1a1a584db534cbbe01735f71dff
Mateus Correia <mateus.correia@outlook.com>
2022-05-23 06:03:45 -0300

    added the function to redirect to stripe when the user click in the subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
-import { stripe } from '../../services/stripe';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -9,12 +10,23 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const {data: session} = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if(!session) {
             signIn('github')
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+            
+            const stripe = await getStripeJs()
+            
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            alert(err.message);
+        } 
     }
     
     return (
